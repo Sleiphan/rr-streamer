bdaf228268f8adaae8776d1f4170309784aa6d9c
Håkon F. Fjellanger <hfredrif@gmail.com>
2024-10-22 13:43:25 +0200

    Make the docker image able to start running detached (option -d)
    This change had the consequence that you cannot start the server unless you have specified all three passwords, but that should not be too much of a problem.

---
+++ icecast/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ICECAST_CONFIG_FILE='./rr-icecast.xml'
+
+ADMIN_PASSWORD_PLACEHOLDER="ADMIN_PASSWORD"
+SOURCE_PASSWORD_PLACEHOLDER="SOURCE_PASSWORD"
+RELAY_PASSWORD_PLACEHOLDER="RELAY_PASSWORD"
+
+# Check if environment variables are set
+if [ -z "$ADMIN_PASSWORD" ]; then
+  echo "Error: ADMIN_PASSWORD is not set."
+  exit 1
+fi
+
+if [ -z "$SOURCE_PASSWORD" ]; then
+  echo "Error: SOURCE_PASSWORD is not set."
+  exit 1
+fi
+
+if [ -z "$RELAY_PASSWORD" ]; then
+  echo "Error: RELAY_PASSWORD is not set."
+  exit 1
+fi
+
+# Replace placeholders in the Icecast config file
+sed -i "s/${ADMIN_PASSWORD_PLACEHOLDER}/$ADMIN_PASSWORD/" ${ICECAST_CONFIG_FILE}
+sed -i "s/${SOURCE_PASSWORD_PLACEHOLDER}/$SOURCE_PASSWORD/" ${ICECAST_CONFIG_FILE}
+sed -i "s/${RELAY_PASSWORD_PLACEHOLDER}/$RELAY_PASSWORD/" ${ICECAST_CONFIG_FILE}
+
+# Start Icecast
+exec su -s /bin/sh -c "icecast -c ${ICECAST_CONFIG_FILE}" icecast
