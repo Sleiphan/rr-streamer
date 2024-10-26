59bef69331491f8e6e2ce53e7a89b128e2ea3829
Håkon F. Fjellanger <hfredrif@gmail.com>
2024-10-26 15:03:38 +0200

    Move icecast xml file to /etc/icecast.xml

--- icecast/setup-icecast.sh
+++ icecast/setup-icecast.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ICECAST_CONFIG_FILE_URL="https://raw.githubusercontent.com/Sleiphan/rr-streamer/refs/heads/main/icecast/rr-icecast.xml"
-ICECAST_CONFIG_FILE="./rr-icecast.xml"
+ICECAST_CONFIG_FILE="/etc/icecast.xml"
 
 ENV_ICECAST_CONFIG_NAME="RR_ICECAST_CONFIG_FILE"
 ADMIN_PASSWORD_PLACEHOLDER="ADMIN_PASSWORD"
@@ -10,25 +10,8 @@ RELAY_PASSWORD_PLACEHOLDER="RELAY_PASSWORD"
 
 ICECAST_START_COMMAND="su -s /bin/sh -c \"nohup icecast -c \$${ENV_ICECAST_CONFIG_NAME} > /dev/null 2>&1 &\" icecast"
 
-# echo -e "\n\n### Welcome to Radio Revolt's setup script for Icecast! ###\n\
-# This script will install, configure, and get the icecast server running for you!\n\n\
-# Since storing passwords in a git repository is something that only knobheads do,\n\
-# I will need you to give me three passwords:\n\n\
-#   # Admin password: A password that the admins (Teknisk) will use to login to the server with administrator privileges.\n\
-#   # Source password: Password that listener software (like radiorevolt.no) will use to access the audio streams.\n\
-#   # Relay password: A password that relay Icecast servers will use to relay the streams from this server. If you are not using any Icecast relays, just enter some gibberish.\n\n\
-# Happy broadcasting!\n"
-
-apk update && apk add wget
-wget ${ICECAST_CONFIG_FILE_URL}
-
-ICECAST_CONFIG_FILE=$(readlink -f ${ICECAST_CONFIG_FILE})
-
-if [ ! -f "${ICECAST_CONFIG_FILE}" ]; then
-  echo -e "Icecast config file not found. \nPlease provide the absolute file path of the Icecast configuration file you want to use:"
-  read ICECAST_CONFIG_FILE
-  echo -e "\n"
-fi
+apk update && apk add wget icecast
+wget -O ${ICECAST_CONFIG_FILE} ${ICECAST_CONFIG_FILE_URL}
 
 echo -e "Setting environment variable ${ENV_ICECAST_CONFIG_NAME}=${ICECAST_CONFIG_FILE}"
 echo "export ${ENV_ICECAST_CONFIG_NAME}=${ICECAST_CONFIG_FILE}" >> /etc/profile
@@ -47,10 +30,6 @@ read RELAY_PASSWORD
 sed -i "s|${SOURCE_PASSWORD_PLACEHOLDER}|${SOURCE_PASSWORD}|g" ${ICECAST_CONFIG_FILE}
 sed -i "s|${ADMIN_PASSWORD_PLACEHOLDER}|${ADMIN_PASSWORD}|g" ${ICECAST_CONFIG_FILE}
 sed -i "s|${RELAY_PASSWORD_PLACEHOLDER}|${RELAY_PASSWORD}|g" ${ICECAST_CONFIG_FILE}
-
-echo -e "Installing Icecast..."
-apk update && apk add icecast > /dev/null 2>&1
-echo -e "DONE\n"
 
 echo -e "Starting icecast in the background..."
 ${ICECAST_START_COMMAND}
